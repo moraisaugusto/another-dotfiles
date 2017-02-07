b9747675060e2356e9ad4470443ed36c55c9edf9
Augusto Morais <augusto@drunken-angel.drunken-angel.local>
2017-02-07 04:05:58 -0200

    urxvt script for toogle menu

--- scripts/bin/urxvt-launcher.sh
+++ scripts/bin/urxvt-launcher.sh
@@ -3,13 +3,17 @@
 wid=$(xdotool search --classname urxvtq)
 
 if [ -z "$wid" ]; then
-  urxvt -name urxvtq -geometry 80x28
+  urxvt -name urxvtq -geometry 80x28 &
+  sleep 0.1
   wid=$(xdotool search --classname urxvtq)
+  xdotool windowactive $wid
+  xdotool windowmove $wid 0 0
   xdotool windowfocus $wid
-  xdotool key Control_L+l
+  #xdotool key Control_L+l
 elif [ -z "$(xdotool search --onlyvisible --classname urxvtq 2>/dev/null)" ]; then
   xdotool windowmap $wid
   xdotool windowfocus $wid
+  xdotool windowmove $wid 0 0
 else
   xdotool windowunmap $wid
 fi
