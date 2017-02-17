777646336716a489adb2b87b09a6d6e3b43f456a
Augusto Morais <aflavio@gmail.com>
2017-02-17 16:50:54 -0200

    script to fix alt keys on urxvt

--- extras/Xresources/fix-alt-key-URxvt.sh
+++ extras/Xresources/fix-alt-key-URxvt.sh
@@ -4,10 +4,19 @@ import locale
 
 locale.setlocale(locale.LC_ALL, "")
 encoding = locale.getlocale()[1]
+ignoredChars = ['f', 'b']
 
+print('!; mapping rxvt keys for alt work on vim')
+print('!; ignored keys: ', end='')
+print(', ' . join(ignoredChars))
+print('!; http://www.unicodemap.org/search.asp?search=e\n\n')
 # ASCII range
 for i in range(32, 128):
 	seq = "".join("\{:o}".format(j) for j in chr(i + (1<<7)).encode(encoding))
 	key = "{:#06X}".format(i)
-	print("urxvt*keysym.Meta-{}: {}".format(key, seq))
+	if chr(i) not in ignoredChars:
+		print("urxvt*keysym.Meta-{}: {}  // char {}".format(key, seq,chr(i)))
+	else:
+		print("!urxvt*keysym.Meta-{}: {}  // char {} REMOVED".format(key, seq,chr(i)))
+	
 
