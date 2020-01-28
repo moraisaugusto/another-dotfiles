373994e7045769e572b49a5225f4c5a61282fee4
Augusto Morais <augustofmorais@gmail.com>
2020-01-28 22:21:14 +0100

    chore: fix to show insync trayicon

--- extras/config/polybar/launch.sh
+++ extras/config/polybar/launch.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q polybar & killall -q insync
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top &
+polybar --reload top &
+insync start &
 #polybar bottom &
 #polybar top-left &
 #polybar top-up &
