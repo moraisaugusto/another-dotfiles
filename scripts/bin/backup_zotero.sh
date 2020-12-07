c7aabd4ddc0c00b6d481e5c140815df2fe092f67
Augusto Morais <augustofmorais@gmail.com>
2020-12-07 22:06:20 +0100

    chore: add backup rotate for zotero script

--- scripts/bin/backup_zotero.sh
+++ scripts/bin/backup_zotero.sh
@@ -3,25 +3,33 @@
 # Backup Zotero data
 date=`date +"%Y%m%d_%H%M%S"`
 tmp_dir=$HOME/.tmp
-
 app="zotero"
+num_backups=5
+notification=1
 zotero_dir="${HOME}/.zotero"
 backup_file="${tmp_dir}/${app}_${date}.tgz"
 cloud_dir="$HOME/Nextcloud/backups/zotero"
-
-echo "Backing up Zotero data... $zotero_dir"
 basedir=`dirname $zotero_dir`
 basename=`basename $zotero_dir`
-notification=1
-
-msg_success="Zotero backup done on $date"
+sucess=false
+msg_success="Zotero backup done in ${app}_${date}.tgz"
 msg_failed="Zotero backup FAILED"
 
-sucess=false
+echo "Backing up Zotero data... $zotero_dir"
 cd $basedir && tar -zcf $backup_file $basename && mv $backup_file $cloud_dir && msg=$msg_success || msg=$msg_failed
 echo $msg
 
-# Pushover notification
+# backup rotate
+num_backups=5
+files=($(ls -t ${cloud_dir}/zotero_*))
+num_files=${#files[@]}
+
+if [ $num_files -gt $num_backups ] ; then
+    for (( i=0; i<$num_backups; i++ )); do unset files[$i] ; done
+    rm ${files[@]}
+fi
+
+# # Pushover notification
 if [ $notification == 1 ] ; then
     source ${HOME}/.zshSecrets
     curl -s \
