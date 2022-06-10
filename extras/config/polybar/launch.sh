766497a778cc4423e431cb8310f8ba014e634fd8
Augusto Morais <augustofmorais@gmail.com>
2022-06-10 22:22:03 +0200

    feat: removed issue while reloading polybar

--- extras/config/polybar/launch.sh
+++ extras/config/polybar/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar & killall -q insync
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+#while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar --reload top &
