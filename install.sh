236d77e63d9f434b80776bd781e43f416f843729
Augusto Morais <augusto@morais.io>
2026-04-06 17:02:45 +0200

    feat: new install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+# -- Repo configuration
+DOTFILES_REPO="https://github.com/moraisaugusto/another-dotfiles"
+DOTFILES_DIR="$HOME/DOTFILES"
+NVIM_REPO="https://github.com/moraisaugusto/nvim"
+NVIM_DIR="$HOME/.config/nvim"
+NVIM_DIR_BACKUP="$HOME/.config/nvim-backup"
+
 set -e
 # setting colors
 RED="$(tput setaf 1)"
@@ -9,8 +17,6 @@ YELLOW="$(tput setaf 3)"
 BLUE="$(tput setaf 4)"
 BOLD="$(tput bold)"
 NORMAL="$(tput sgr0)"
-
-
 
 
 # Function to draw progress bar
@@ -37,29 +43,6 @@ progressBar () {
   printf "%${empty}s" "" | sed 's/ /\xE2\x96\x91/g'
   printf "] $percent%% - $text "
 }
-
-
-
-
-
-## Collect task count
-#taskCount=33
-#tasksDone=0
-#
-#while [ $tasksDone -le $taskCount ]; do
-#
-#  # Do your task
-#  (( tasksDone += 1 ))
-## Add some friendly output
-#  text=$(echo "somefile-$tasksDone.dat")
-#
-#  # Draw the progress bar
-#  progressBar $taskCount $taskDone $text
-#
-#  sleep 0.01
-#done
-#
-#echo
 
 
 checkShell() {
@@ -95,16 +78,6 @@ checkRequisites() {
     checkCmds "${commands[@]}"
 }
 
-checkDirectories() {
-    local_dirs=("$@")
-
-    for (( i = 0 ; i < ${#local_dirs[@]} ; i++ ))
-        do
-            if [ ! -d "$i" ]; then
-                mkdir ${local_dirs[$i]}
-            fi
-    done
-}
 promptConfirm() {
   while true; do
     read -r -n 1 -p "${1}" REPLY
@@ -129,88 +102,77 @@ checkSymlink() {
     fi
 }
 
-
-
-loaderSpinner()
-{
+loaderSpinner() {
     local pid=$!
-    local delay=0.5
-    local spinstr='|/-\'
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-        local temp=${spinstr#?}
-        printf "\t [%c%c] $1" "$spinstr" "$spinstr"
-        local spinstr=$temp${spinstr%"$temp"}
-        sleep $delay
-        printf '\b%.0s' {1..60}
+    local msg=$1
+    local delay=0.1
+    # Braille frames from the 'dots' spinner style
+    local frames=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
+    
+    # Hide the cursor so it doesn't flicker
+    tput civis 
+
+    while kill -0 "$pid" 2>/dev/null; do
+        for frame in "${frames[@]}"; do
+            # \r moves cursor to start, \033[K clears the line
+            printf "\r \033[36m%s\033[0m %s" "$frame" "$msg"
+            sleep "$delay"
+            
+            # Check again inside the loop to finish instantly when the PID dies
+            if ! kill -0 "$pid" 2>/dev/null; then
+                break
+            fi
+        done
     done
-    printf "\t${BOLD}${GREEN} [OK] ${NORMAL}$1\n"
+
+    # Restore cursor and print final status
+    tput cnorm
+    printf "\r \033[32m✔\033[0m %s\n" "$msg"
 }
 
 
 
 main() {
     printf "\n${BOLD}[INSTALLING] ${NORMAL}\n"
-    # vars
-    DOTFILES=$HOME/.dotfiles
-
-    checkRequisites
-
-    # create directories
-    directories=(".dotfiles" ".fonts")
-
-    # checking directories
-    checkDirectories "${directories[@]}" & loaderSpinner " Checking directories..."
-
-    (git clone -q https://github.com/aflavio/another-dotfiles.git .dotfiles) & loaderSpinner " Clonning another dotfiles..."
-
-    cd .dotfiles
-    (git submodule update --init --recursive)  & loaderSpinner " Clonning submodules (including oh-my-zsh)..."
-    cd ..
-
-    # creating symlinks
-    checkSymlink $DOTFILES/vim $HOME/.vim
-    checkSymlink $DOTFILES/fonts .fonts
-    checkSymlink $DOTFILES/extras/oh-my-zsh/custom/themes $DOTFILES/oh-my-zsh/custom/themes
-    checkSymlink $DOTFILES/extras/oh-my-zsh/custom/plugins/tmux $DOTFILES/oh-my-zsh/custom/plugins/tmux
-    checkSymlink $DOTFILES/symlinkis/zshAlias.symlink $HOME/.zshAlias
-
-    linkables=$( find -H "$DOTFILES" -name '*.symlink' )
-    for file in $linkables ; do
-        target="$HOME/.$( basename $file ".symlink" )"
-        checkSymlink $file $target
-    done
-    mv $HOME/.tmux $HOME/.tmux.conf
-
-    printf "${BOLD}${GREEN}\t [OK] ${NORMAL} Creating symlinks...${NORMAL}\n"
-
-    #source $HOME/.zshrc
-
-    ## copying init.d script files
-    #promptConfirm "Do you want to install extra scripts (needs root permission)? (y/n): " || exit 0
-    #sudo checkSymlink $DOTFILES/scripts/init.d/reset-ethernet.sh /etc/init.d/reset-ethernet.sh
+    #
+    (git clone -q $DOTFILES_REPO $DOTFILES_DIR) & loaderSpinner " Clonning another dotfiles..."
+    #
+    cd $DOTFILES_DIR 
+    (git submodule update --init --recursive -q)  & loaderSpinner " Clonning submodules (including oh-my-zsh)..."
 }
 
 vimInstallation() {
     printf "\n"
-    promptConfirm "Do you want to install vim dotfiles? (y/n) " || exit 0
+    promptConfirm "Do you want to install vim configuration? (y/n) " || exit 0
 
-    printf "\n${BOLD}${YELLOW}[WARNING] ${NORMAL}${BOLD}Sometimes when clonning vim plugin the installation can broke.\n"
-    printf "${NORMAL}\t  Then you need to run this command manually:\n"
-    printShellCmd "vim +PlugInstall"
-    printf "${NORMAL}Press any key to continue\n"
-    read pause
+    printf "\n${BOLD}${BLUE}[NOTE]\n"
+    printf "${NORMAL}Vim configuration will be cloned in your ${BOLD}$HOME/.config/nvim folder.\n"
 
-    vim +PlugInstall +qall
+    if [ ! -d "$NVIM_DIR" ]; then
+
+      printf "${NORMAL}Press any key to continue or abort (ctrl+c)\n"
+      read pause
+      (git clone $NVIM_REPO $NVIM_DIR -q) & loaderSpinner " Clonning another vim configuration..."
+
+    else
+      printf "\n${BOLD}${YELLOW}[REQUIREMENT]\n"
+      printf "${NORMAL}${BOLD}$HOME/.config/nvim${NORMAL} folder is not empty.\n"
+      printf "\n"
+      promptConfirm "${NORMAL}Do you want to back up your current vim configuration? (y/n) " || exit 0
+
+      mv $NVIM_DIR $NVIM_DIR_BACKUP
+      (git clone $NVIM_REPO $NVIM_DIR -q) & loaderSpinner " Clonning another vim configuration..."
+    fi
 }
 
 suggestionMessage() {
     printf "\n"
     printf "\n${BOLD}${BLUE}[SUGGESTIONS] \n"
-    printf "${NORMAL}\t\xE2\x97\x86 ${NORMAL} Use ${BOLD}'Source Code Pro for Powerline'${NORMAL} as terminal font\n"
-    printf "\t\xE2\x97\x86 ${BOLD} Logout from all sessions${NORMAL} to reload your .zshrc \n"
-    printf "\t   (sometimes only source ./zshrc doesn't work well)'\n"
-    printf "${BOLD}\t\xE2\x97\x86 ${NORMAL} If you want, you can install Vim Plugins ${BOLD}in the end \n"
-    printf "\t   of this installation \n"
+    printf "${NORMAL}\xE2\x97\x86 ${NORMAL} Use ${BOLD}'Source Code Pro for Powerline'${NORMAL} as terminal font\n"
+    printf "\xE2\x97\x86 ${BOLD} Logout from all sessions${NORMAL} to reload your .zshrc \n"
+    printf "   (sometimes only source ./zshrc doesn't work well)'\n"
+    printf "${BOLD}\xE2\x97\x86 ${NORMAL} If you want, you can install Vim Plugins ${BOLD}in the end \n"
+    printf "   of this installation \n"
     printf "${NORMAL}\n"
 }
 
@@ -236,7 +198,7 @@ splash() {
 bye() {
     printf "\n${BOLD}${GREEN}[SUCCESS]\n\t${NORMAL} ${BOLD}Now you have the dotfiles configured!${NORMAL}\n"
     printf "${BLUE}${BOLD}\t Thanks for installing Another Dotfiles\n"
-    printf "${BOLD}\t Contact: ${NORMAL}${BOLD}aflavio at gmail.com${NORMAL}\n\n"
+    printf "${BOLD}\t Contact: ${NORMAL}${BOLD}hi@morais.io${NORMAL}\n\n"
 }
 
 
