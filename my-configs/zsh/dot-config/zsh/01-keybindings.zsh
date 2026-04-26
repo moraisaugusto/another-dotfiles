5ba813ea5e13cf4a4c565b96c9e77c6aaf8ee619
Augusto Morais <augusto@morais.io>
2026-04-27 00:19:41 +0200

    chore: navigate on zsh history with up and down keys

--- my-configs/zsh/dot-config/zsh/01-keybindings.zsh
+++ my-configs/zsh/dot-config/zsh/01-keybindings.zsh
@@ -6,3 +6,17 @@ bindkey '^P' up-line-or-history
 
 # Bind Ctrl+N to Next command
 bindkey '^N' down-line-or-history
+
+# Native History Substring Search Logic
+# This binds Up/Down arrows to search history based on what is already typed
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+# Keybindings for standard Arrow Keys
+bindkey '^[[A' up-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search
+
+# Keybindings for Vi-mode (if you use it) or specific terminal emulators
+bindkey -M emacs '^[[A' up-line-or-beginning-search
+bindkey -M emacs '^[[B' down-line-or-beginning-search
