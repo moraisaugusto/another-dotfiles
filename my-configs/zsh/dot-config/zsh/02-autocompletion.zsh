3b2bc1ed00e43bc70ef1a5ebafa381d8c6bec0f6
Augusto Morais <augusto@morais.io>
2026-04-26 20:22:59 +0200

    feat: add background while completation

--- my-configs/zsh/dot-config/zsh/02-autocompletion.zsh
+++ my-configs/zsh/dot-config/zsh/02-autocompletion.zsh
@@ -1,22 +1,33 @@
-#█▓▒░ autocompletion systems
+#█▓▒░ 1. Autocompletion System Setup
+# Setup fpath and initialize completion systems
 loc=${ZDOTDIR:-"$HOME/.config/zsh"}
 fpath=($loc/completion $fpath)
-autoload bashcompinit && bashcompinit
-autoload -Uz compinit && compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
 
-#█▓▒░ style
-zstyle ':completion:*' auto-description 'specify: %d'
+autoload -Uz compinit && compinit -d "$XDG_CACHE_HOME"/zsh/zcompdump-"$ZSH_VERSION"
+autoload bashcompinit && bashcompinit
+
+#█▓▒░ 2. Completion Behavior & Matching
+# Define how completions are generated and matched
 zstyle ':completion:*' completer _expand _complete _correct _approximate
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' menu select=2 eval "$(dircolors -b)"
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %p: hit TAB for more, or the character to insert%s
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
-zstyle ':completion:*' menu select=long
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
 zstyle ':completion:*' use-compctl false
 zstyle ':completion:*' verbose true
+
+#█▓▒░ 3. Menu & Interface
+# Configuration for the interactive TAB menu
+zstyle ':completion:*:*:*:*:*' menu select=2
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-prompt %SAt %p: hit TAB for more, or the character to insert%s
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+
+#█▓▒░ 4. Visuals & Colors
+# Highlighting and directory colors
+# ma=44;30;1 -> Blue background, Black text, Bold
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} "ma=44;30;1"
+zstyle ':completion:*' list-colors 'di=01;38;5;208'
+
+#█▓▒░ 5. Command Specifics
+# Custom behavior for specific commands
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,cmd'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
